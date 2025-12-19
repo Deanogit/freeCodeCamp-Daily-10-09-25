866e1ae022f60f275c1cfcc9e2048ecf5e1b86cb
Dean Howe <hideanhowe@gmail.com>
2025-12-19 10:02:36 +0000

    testing

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // Array Diff
 // Given two arrays with strings values, return a new array containing all the values that appear in only one of the arrays.
 
-// The returned array should be sorted in alphabetical order
+// The returned array should be sorted in alphabetical order.
+
+function arrayDiff(arr1, arr2) {
+  // console.log(arr1, arr2)
+  const obj = {};
+  // add both arr to an object
+  function pusher(x) {
+    for (i = 0; i < x.length; i++) {
+      obj.add(x, (obj.has(x) || 0) + 1);
+    }
+  }
+
+  pusher(arr1);
+  pusher(arr2);
+  console.log(obj);
+  // store number of occurences
+  // if occurence === 1 push to result arr
+  //return arr1;
+}
